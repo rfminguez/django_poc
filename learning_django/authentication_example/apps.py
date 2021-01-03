f0e9592125998471cf84f742ef4dce64a5d502fd
rfminguez <rfminguez@gmail.com>
2021-01-03 14:38:15 +0100

    add: basic authentication example

---
+++ learning_django/authentication_example/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AuthenticationExampleConfig(AppConfig):
+    name = 'authentication_example'
--- learning_django/learning_django/urls.py
+++ learning_django/learning_django/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
+    path('authentication_example/', include("authentication_example.urls")),
     path('admin/', admin.site.urls),
 ]
